d72eb1bb4809a117c63691d30350c33f62cd131a
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-01-10 22:35:18 +0900

    FineDustApp: API Data Ues & Save - 2

--- lib/data/fine_dust.dart
+++ lib/data/fine_dust.dart
@@ -2,24 +2,24 @@ class FineDust {
   int? pm10;
   int? pm25;
   int? khai;
-  String? dateTime;
+  String? dataTime;
   double? so;
   double? co;
   double? no;
   double? o3;
 
-  FineDust({this.pm10, this.pm25, this.khai, this.dateTime, this.so, this.co, this.no, this.o3});
+  FineDust({this.pm10, this.pm25, this.khai, this.dataTime, this.so, this.co, this.no, this.o3});
 
   factory FineDust.fromJson(Map<String, dynamic> data) {
     return FineDust(
       pm10: int.tryParse(data["pm10Value"] ?? "") ?? 0,
       pm25: int.tryParse(data["pm25Value"] ?? "") ?? 0,
       khai: int.tryParse(data["khaiValue"] ?? "") ?? 0,
-      dateTime: data["dataTime"] ?? "",
-      so: data["so2Value"] ?? "",
-      co: data["coValue"] ?? "",
-      no: data["no2Value"] ?? "",
-      o3: data["o3Value"] ?? "",
+      dataTime: data["dataTime"] ?? "",
+      so: double.tryParse(data["so2Value"] ?? "") ?? 0.0,
+      co: double.tryParse(data["coValue"] ?? "") ?? 0.0,
+      no: double.tryParse(data["no2Value"] ?? "") ?? 0.0,
+      o3: double.tryParse(data["o3Value"] ?? "") ?? 0.0,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:fine_dust_app/data/api.dart';
+import 'package:fine_dust_app/data/fine_dust.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,30 +29,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  List<Color> colors = const [
+    Color(0xFF32A1FF),
+    Color(0xFF00C73C),
+    Color(0xFFFD9B5A),
+    Color(0xFFFF5959),
+  ];
+  List<String> icon = const [
+    'assets/img/angry.png',
+    'assets/img/bad.png',
+    'assets/img/happy.png',
+    'assets/img/sad.png',
+  ];
+  List<String> status = const [
+    '좋음',
+    '보통',
+    '나쁨',
+    '매우나쁨',
+  ];
+  String stationName = "종로구";
+  List<FineDust> data = [];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          ''
-        ),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          FineDustApi api = FineDustApi();
+          data = await api.getFineDustData("종로구");
+          data.removeWhere((element) =>  element.pm10 == 0);
+          for (final d in data) {
+            print("${d.dataTime} ${d.pm10}");
+          }
+        },
         child: const Icon(Icons.add),
       ),
     );
